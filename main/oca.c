67a83b3be68754b883e1b0b6f4141e858ad8a5f3
mick999888 <michael.karl.bayern@gmx.net>
2025-02-01 23:50:00 +0100

    insterted timers and MQTT routine

--- main/oca.c
+++ main/oca.c
@@ -1,30 +1,30 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "driver/adc.h"
 #include "driver/gpio.h"
-#include "esp_log.h"
-#include "esp_adc_cal.h"
-#include "esp_timer.h"
-#include "rom/ets_sys.h"
-#include "freertos/semphr.h"
-#include "driver/timer.h"
 #include "driver/spi_master.h"
+#include "driver/spi_master.h"
+#include "driver/timer.h"
+#include "esp_adc_cal.h"
+#include "esp_check.h"
 #include "esp_eth.h"
 #include "esp_eth_mac.h"
-#include "esp_netif.h"
-#include "esp_event.h"
-#include "nvs_flash.h"
-#include "driver/spi_master.h"
-#include "esp_log.h"
-#include "mqtt_client.h"
 #include "esp_eth_mac.h"
 #include "esp_eth_phy.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_log.h"
+#include "esp_netif.h"
 #include "esp_task_wdt.h"
-#include "esp_check.h"
+#include "esp_timer.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+#include "mqtt_client.h"
+#include "nvs_flash.h"
+#include "rom/ets_sys.h"
 #include "rtc_wdt.h"
 
-#include "globals.h"
 #include "defines.h"
+#include "globals.h"
 #include "IR_Send.h"
 #include "SW_Function.h"
 
@@ -39,11 +39,6 @@ static void log_error_if_nonzero(const char *message, int error_code)
         ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
     }
 }
-
-//HINT: Function esp_eth_mac_new_esp32() has been refactored to accept device specific configuration and MAC specific configuration.
-//Please refer to the Ethernet section of Networking migration guide for more details.
-//HINT: Function esp_eth_phy_new_lan8720() has been removed, please use esp_eth_phy_new_lan87xx() instead.
-//Please refer to the Networking migration guide, section PHY Address Auto-detect, for more details.
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // Semaphore + ISR hanlde setup
@@ -63,13 +58,10 @@ esp_timer_handle_t ISR_SUB2;
 esp_timer_handle_t ISR_MAIN1;
 esp_timer_handle_t ISR_MAIN2;
 
-
+///////////////////////////////////////////////////////////////////////////////////////////
+// Timer routines
 ////////////////////////////////////////////////////////////////////////////////////////////
-// Interrupt routines
-////////////////////////////////////////////////////////////////////////////////////////////
-
-/*
-void IRAM_ATTR ISR_TIMER_MAIN1(void* arg)
+static void IRAM_ATTR ISR_TIMER_MAIN1(void* arg)
 {
   i_2_IN    = 0;
   i_3_SUB1  = 0;
@@ -81,7 +73,7 @@ void IRAM_ATTR ISR_TIMER_MAIN1(void* arg)
   IR_Start();  
 }
 
-void IRAM_ATTR ISR_TIMER_MAIN2(void* arg)
+static void IRAM_ATTR ISR_TIMER_MAIN2(void* arg)
 {
   i_2_IN    = 0;
   i_9_SUB2  = 0;
@@ -93,7 +85,7 @@ void IRAM_ATTR ISR_TIMER_MAIN2(void* arg)
   IR_Start();      
 }
 
-void IRAM_ATTR ISR_TIMER_SUB1(void* arg)
+static void IRAM_ATTR ISR_TIMER_SUB1(void* arg)
 {
     // go on
    if ((i_4_HALT1 == 1) && (i_6_MAIN1 == 0)) { // Belegtmelder auf Hauptstrecke muss "0" sein
@@ -120,105 +112,11 @@ void IRAM_ATTR ISR_TIMER_SUB2(void* arg)
    // vTaskDelay(1 / portTICK_RATE_MS);
 
 }
-
-void IRAM_ATTR ISR_8_BUTTON(void* arg) // Start Knopf
-{
-	i_8_BUTTON = 1;
-}
-
-void IRAM_ATTR ISR_7_MAIN2(void* arg)  // Main2
-{
-    bool is_active;
-
-	i_7_MAIN2 = 1;
-    is_active = esp_timer_is_active(ISR_TIMER_MAIN2);
-    if (is_active) {
-      esp_timer_stop(ISR_TIMER_MAIN2);
-      esp_timer_start_periodic(ISR_TIMER_MAIN2, 200000);
-    } else {
-      esp_timer_start_periodic(ISR_TIMER_MAIN2, 200000);   // Belegtmelder #1, 2 sek pause
-    }
-}
-
-void IRAM_ATTR ISR_6_MAIN1(void* arg)  // Main1
-{
-    bool is_active;
-
-	i_6_MAIN1 = 1;
-    is_active = esp_timer_is_active(ISR_TIMER_MAIN1);
-    if (is_active) {
-      esp_timer_stop(ISR_TIMER_MAIN1);
-      esp_timer_start_periodic(ISR_TIMER_MAIN1,200000);
-    } else {
-      esp_timer_start_periodic(ISR_TIMER_MAIN1, 200000);
-    }
-
-    esp_timer_start_periodic(ISR_TIMER_MAIN1, 200000);    // Belegtmelder #2, 2 sek pause
-}
-
-void IRAM_ATTR ISR_5_HALT2(void* arg) // Halt2
-{
-	i_5_HALT2 = 1;
-	IR_Blinker_aus();
-	IR_Stop();
-
-	//ESP_ERROR_CHECK_WITHOUT_ABORT(esp_timer_start_periodic(ISR_SUB1, 6000000));
-	esp_intr_disable(ISR_5); // save CPU load
-    esp_intr_disable(ISR_9); // save CPU load
-	esp_timer_start_periodic(ISR_SUB2, 6000000); // 1 min 
-
-    // full house
-    if ((i_5_HALT2 == 1) && (i_4_HALT1 == 1)) {
-        esp_intr_disable(ISR_1);
-        esp_intr_disable(ISR_2);        
-    }
-}
-
-void IRAM_ATTR ISR_4_HALT1(void* arg) // Halt1
-{
-	i_4_HALT1 = 1;
-	IR_Blinker_aus();
-	IR_Stop();
-
-	esp_intr_disable(ISR_3); // save CPU load
-    esp_intr_disable(ISR_4); // save CPU load
-	esp_timer_start_periodic(ISR_SUB1, 6000000); // 1 min 
-
-    // full house
-    if ((i_5_HALT2 == 1) && (i_4_HALT1 == 1)) {
-        esp_intr_disable(ISR_1);
-        esp_intr_disable(ISR_2);        
-    }    
-}
-
-void IRAM_ATTR ISR_9_SUB2(void* arg) // Sub2 Einfahrt
-{
-    i_9_SUB2 = 1;
-
-	// falls SUB1 frei ist
-	if (i_3_SUB1 == 0)
-	    SW_Sub1(i_SW_L);
-    
-    esp_intr_enable(ISR_5);
-}
-
-void IRAM_ATTR ISR_3_SUB1(void* arg) // Sub1 Einfahrt
-{
-	i_3_SUB1 = 1;
-
-    // falls SUB2 frei ist
-	if (i_9_SUB2 == 0)
-	    SW_Sub1(i_SW_R);
-
-    esp_intr_enable(ISR_4);
-}
-
-
-*/
-
+////////////////////////////////////////////////////////////////////////////////////////////
+// Interrupt routines
+////////////////////////////////////////////////////////////////////////////////////////////
 static void IRAM_ATTR ISR_1_Einfahrt(void* args) 
 {
-
     uiCurrentTime = esp_timer_get_time();
     uiTimeBetweenInterrupts = uiCurrentTime - uiLastInterruptTime;
 
@@ -313,14 +211,9 @@ static void IRAM_ATTR ISR_1_Einfahrt(void* args)
 				//    xSemaphoreGive(xMutex);
 			    //}
 	        }            
-
         }
-
     }
-
     uiLastInterruptTime = uiCurrentTime;
-    
-
 }
 
 static void IRAM_ATTR ISR_2_IN(void* arg)  // Einfahrt - Weiche zurücksetzen
@@ -382,6 +275,72 @@ static void IRAM_ATTR ISR_4_HALT1(void* arg) // Halt1
         esp_intr_disable(ISR_1);
         esp_intr_disable(ISR_2);
     }
+}
+
+static void IRAM_ATTR ISR_5_HALT2(void* arg) // Halt2
+{
+    i_5_HALT2 = 1;
+    IR_Blinker_aus();
+    IR_Stop();
+
+    //ESP_ERROR_CHECK_WITHOUT_ABORT(esp_timer_start_periodic(ISR_SUB1, 6000000));
+    esp_intr_disable(ISR_5); // save CPU load
+    esp_intr_disable(ISR_9); // save CPU load
+    esp_timer_start_periodic(ISR_SUB2, 6000000); // 1 min 
+
+    // full house
+    if ((i_5_HALT2 == 1) && (i_4_HALT1 == 1)) {
+        esp_intr_disable(ISR_1);
+        esp_intr_disable(ISR_2);
+    }
+}
+
+static void IRAM_ATTR ISR_6_MAIN1(void* arg)  // Main1
+{
+    bool is_active;
+
+    i_6_MAIN1 = 1;
+    is_active = esp_timer_is_active(ISR_TIMER_MAIN1);
+    if (is_active) {
+        esp_timer_stop(ISR_TIMER_MAIN1);
+        esp_timer_start_periodic(ISR_TIMER_MAIN1, 200000);
+    }
+    else {
+        esp_timer_start_periodic(ISR_TIMER_MAIN1, 200000);
+    }
+
+    esp_timer_start_periodic(ISR_TIMER_MAIN1, 200000);    // Belegtmelder #2, 2 sek pause
+}
+
+static void IRAM_ATTR ISR_7_MAIN2(void* arg)  // Main2
+{
+	bool is_active;
+
+	i_7_MAIN2 = 1;
+	is_active = esp_timer_is_active(ISR_TIMER_MAIN2);
+	if (is_active) {
+		esp_timer_stop(ISR_TIMER_MAIN2);
+		esp_timer_start_periodic(ISR_TIMER_MAIN2, 200000);
+	}
+	else {
+		esp_timer_start_periodic(ISR_TIMER_MAIN2, 200000);   // Belegtmelder #1, 2 sek pause
+	}
+}
+
+static void IRAM_ATTR ISR_8_BUTTON(void* arg) // Start Knopf
+{
+	i_8_BUTTON = 1;
+}
+
+static void IRAM_ATTR ISR_9_SUB2(void* arg) // Sub2 Einfahrt
+{
+	i_9_SUB2 = 1;
+
+	// falls SUB1 frei ist
+	if (i_3_SUB1 == 0)
+		SW_Sub1(i_SW_L);
+
+	esp_intr_enable(ISR_5);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -459,8 +418,6 @@ void ISR_4_HALT1_handler(void* pvParameters)
     io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
     io_conf.pull_down_en = GPIO_PULLUP_ENABLE;
     gpio_config(&io_conf);
-
-    //ESP_ERROR_CHECK(gpio_install_isr_service(0));
     ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO17_I_HALT1_ISR_4, ISR_2_IN, GPIO17_I_HALT1_ISR_4));
 
     while (1) {
@@ -468,11 +425,158 @@ void ISR_4_HALT1_handler(void* pvParameters)
     }
 }
 
+void ISR_5_HALT2_handler(void* pvParameters)
+{
+    gpio_config_t io_conf;
+
+    // Configure GPIO34_I_HALT2_ISR_5
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    io_conf.pin_bit_mask = (1ULL << GPIO34_I_HALT2_ISR_5);
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLUP_ENABLE;
+    gpio_config(&io_conf);
+    ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO17_I_HALT1_ISR_4, ISR_2_IN, GPIO17_I_HALT1_ISR_4));
+
+    while (1) {
+        vTaskDelay(1);
+    }
+}
+
+void ISR_6_MAIN1_handler(void* pvParameters)
+{
+	gpio_config_t io_conf;
+
+	// Configure GPIO22_I_MAIN1_ISR6
+	io_conf.intr_type = GPIO_INTR_POSEDGE;
+	io_conf.pin_bit_mask = (1ULL << GPIO22_I_MAIN1_ISR6);
+	io_conf.mode = GPIO_MODE_INPUT;
+	io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+	io_conf.pull_down_en = GPIO_PULLUP_ENABLE;
+	gpio_config(&io_conf);
+	ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO22_I_MAIN1_ISR6, ISR_2_IN, GPIO22_I_MAIN1_ISR6));
+
+	while (1) {
+		vTaskDelay(1);
+	}
+}
+
+void ISR_7_MAIN2_handler(void* pvParameters)
+{
+	gpio_config_t io_conf;
+
+	// Configure GPIO36_I_MAIN2_ISR7
+	io_conf.intr_type = GPIO_INTR_POSEDGE;
+	io_conf.pin_bit_mask = (1ULL << GPIO36_I_MAIN2_ISR7);
+	io_conf.mode = GPIO_MODE_INPUT;
+	io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+	io_conf.pull_down_en = GPIO_PULLUP_ENABLE;
+	gpio_config(&io_conf);
+	ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO36_I_MAIN2_ISR7, ISR_2_IN, GPIO36_I_MAIN2_ISR7));
+
+	while (1) {
+		vTaskDelay(1);
+	}
+}
+
+void ISR_8_BUTTON_handler(void* pvParameters)
+{
+	gpio_config_t io_conf;
+
+	// Configure GPIO39_I_BUTTON_ISR8
+	io_conf.intr_type = GPIO_INTR_POSEDGE;
+	io_conf.pin_bit_mask = (1ULL << GPIO39_I_BUTTON_ISR8);
+	io_conf.mode = GPIO_MODE_INPUT;
+	io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+	io_conf.pull_down_en = GPIO_PULLUP_ENABLE;
+	gpio_config(&io_conf);
+	ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO39_I_BUTTON_ISR8, ISR_2_IN, GPIO39_I_BUTTON_ISR8));
+
+	while (1) {
+		vTaskDelay(1);
+	}
+}
+
+void ISR_9_SUB2_handler(void* pvParameters)
+{
+	gpio_config_t io_conf;
+
+	// Configure GPIO35_I_SUB2_ISR_9
+	io_conf.intr_type = GPIO_INTR_POSEDGE;
+	io_conf.pin_bit_mask = (1ULL << GPIO35_I_SUB2_ISR_9);
+	io_conf.mode = GPIO_MODE_INPUT;
+	io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+	io_conf.pull_down_en = GPIO_PULLUP_ENABLE;
+	gpio_config(&io_conf);
+	ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO35_I_SUB2_ISR_9, ISR_2_IN, GPIO35_I_SUB2_ISR_9));
+
+	while (1) {
+		vTaskDelay(1);
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////
+// MQTT setup
+////////////////////////////////////////////////////////////////////////////////////////////
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        ESP_LOGI(TAG, "TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        ESP_LOGI(TAG, "DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
 ////////////////////////////////////////////////////////////////////////////////////////////
 // Main
 ////////////////////////////////////////////////////////////////////////////////////////////
-
-
 void app_main(void) 
 {
     ////////////////////////////////////////////////////////////////////////////////////////////
@@ -522,13 +626,18 @@ void app_main(void)
 
     //  create here tasks
     vTaskDelay (1000/portTICK_PERIOD_MS);
+
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
-    xTaskCreatePinnedToCore(ISR_1_Einfahrt_handler, "ISR_1_Einfahrt_handler", 2048, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(ISR_2_IN_handler,       "ISR_2_IN_handler",       2048, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(ISR_3_SUB1_handler,     "ISR_3_SUB1_handler", 2048, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(ISR_4_HALT1_handler,    "ISR4_HALT1_handler", 2048, NULL, 5, NULL, 0);
-
-
+    xTaskCreatePinnedToCore(ISR_1_Einfahrt_handler, "ISR_1_Einfahrt_handler",   2048, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(ISR_2_IN_handler,       "ISR_2_IN_handler",         2048, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(ISR_3_SUB1_handler,     "ISR_3_SUB1_handler",       2048, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(ISR_4_HALT1_handler,    "ISR4_HALT1_handler",       2048, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(ISR_5_HALT2_handler,    "ISR5_HALT2_handler",       2048, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(ISR_6_MAIN1_handler,    "ISR6_MAIN1_handler",       2048, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(ISR_7_MAIN2_handler,    "ISR7_MAIN2_handler",       2048, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(ISR_8_BUTTON_handler,   "ISR8_BUTTON_handler",      2048, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(ISR_9_SUB2_handler,     "ISR9_SUB2_handler",        2048, NULL, 5, NULL, 0);
+    
 
     // clear beginning - set all interrupts online
     esp_intr_enable(ISR_1);
